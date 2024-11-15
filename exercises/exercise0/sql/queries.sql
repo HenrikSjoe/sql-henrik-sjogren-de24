8cc5c6ae331c88af802c2e7577741ab290406589
Henrik <sjogren.henrik@gmail.com>
2024-11-15 11:57:17 +0100

    added excercise 0

---
+++ exercises/exercise0/sql/queries.sql
@@ -0,0 +1,19 @@
+SELECT * FROM sql_course;
+
+SELECT * FROM sql_course WHERE content_type = 'exercise';
+
+SELECT * FROM sql_course WHERE content_type = 'lecture';
+
+SELECT * FROM sql_course WHERE week = 48;
+
+SELECT * FROM sql_course WHERE week BETWEEN 47 AND 49;
+
+SELECT COUNT(content_type) from main.sql_course WHERE content_type = 'lecture';
+
+SELECT COUNT(DISTINCT content_type) FROM main.sql_course; 
+
+SELECT DISTINCT content_type FROM main.sql_course;
+
+
+
+
