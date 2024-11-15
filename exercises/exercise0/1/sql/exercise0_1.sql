382a2375960eeecc06a9b5b312ae7ad27c237280
Henrik <sjogren.henrik@gmail.com>
2024-11-15 15:13:25 +0100

    restructured exercise 0.1

--- exercises/exercise0/1/sql/exercise0_1.sql
+++ exercises/exercise0/1/sql/exercise0_1.sql
@@ -26,8 +26,13 @@ ORDER BY
 	final_price DESC
 LIMIT 5;
 -- f)
-SELECT * FROM hemnet_data ORDER BY final_price LIMIT 5;
-
+SELECT
+	*
+FROM
+	hemnet_data
+ORDER BY
+	final_price
+LIMIT 5;
 -- g)
 SELECT 
 	MIN(final_price) as min_price,
@@ -36,7 +41,6 @@ SELECT
 	MAX(final_price) as max_price
 FROM
 	hemnet_data;
-
 -- h)
 SELECT 
 	MIN(price_per_area) as min_price,
@@ -45,18 +49,15 @@ SELECT
 	MAX(price_per_area) as max_price
 FROM
 	hemnet_data;
-	
 --i)
-SELECT COUNT(DISTINCT commune) FROM hemnet_data; 
-
+SELECT
+	COUNT(DISTINCT commune)
+FROM
+	hemnet_data;
 -- j)
 SELECT
 	(COUNT(final_price) FILTER (
-	WHERE final_price > 10000000) * 100.0) / COUNT(final_price) AS percentage_above_10M
+WHERE
+	final_price > 10000000) * 100.0) / COUNT(final_price) AS percentage_above_10M
 FROM
-	main.hemnet_data; 
-
-
-
-
-
+	main.hemnet_data;
