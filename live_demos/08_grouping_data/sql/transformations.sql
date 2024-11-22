71ed93a776486f15eaa4c490a124f8ad32cd42e2
Henrik <sjogren.henrik@gmail.com>
2024-11-22 12:38:25 +0100

    added 08_grouping_data

---
+++ live_demos/08_grouping_data/sql/transformations.sql
@@ -0,0 +1,21 @@
+SELECT * FROM main.food ;
+
+CREATE TABLE IF NOT EXISTS cleaned_food AS (
+SELECT
+	id AS food,
+	week_id AS week,
+	SUBSTRING(week, 1, 4) AS year,
+	value AS number_searches
+FROM
+	main.food );
+	
+
+SELECT
+	id AS food,
+	week_id AS week,
+	SUBSTRING(week, 1, 4) AS year,
+	week[:4] AS year
+FROM
+	main.food ;
+	
+SELECT * FROM main.cleaned_food;
