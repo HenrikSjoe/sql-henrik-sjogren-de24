a29c44f4b55272e787d069cec00a65798ab3e442
Henrik <sjogren.henrik@gmail.com>
2024-11-22 10:33:30 +0100

    added 06_crud_operations

---
+++ live_demos/06_crud_operations/sql/2_crud_read.sql
@@ -0,0 +1,7 @@
+SELECT * FROM database.sql;
+
+SELECT * FROM programming.python;
+
+SELECT * FROM database.duckdb;
+
+
---
+++ live_demos/06_crud_operations/sql/4_crud_delete.sql
@@ -0,0 +1,25 @@
+SELECT
+	*
+FROM
+	programming.python
+WHERE
+	id = 2;
+	
+DELETE
+FROM
+	programming.python
+WHERE
+	id = 2;
+	
+SELECT
+	*
+FROM
+	database.duckdb d
+WHERE
+	id > 10;
+	
+DELETE
+FROM
+	database.duckdb
+WHERE
+	id > 10;
